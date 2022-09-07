eee3201783424290c5b39c401e324258f48505c9
unknown <p.satheesh@ahex.co.in>
2022-09-07 13:43:42 +0530

    fixed the issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,7 @@ import { AnimatedgirlComponent } from './animatedgirl/animatedgirl.component';
 import { ModelComponent } from './model/model.component';
 import { ControlsComponent } from './controls/controls.component';
 import { FormsModule } from '@angular/forms';
-import { TestingComponent } from './testing/testing.component';
-import { Testing2Component } from './testing2/testing2.component';
-import { Testing3Component } from './testing3/testing3.component';
+
 
 @NgModule({
   declarations: [
@@ -19,9 +17,6 @@ import { Testing3Component } from './testing3/testing3.component';
     AnimatedgirlComponent,
     ModelComponent,
     ControlsComponent,
-    TestingComponent,
-    Testing2Component,
-    Testing3Component
   ],
   imports: [
     BrowserModule,
